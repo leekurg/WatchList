e522a8962d78249d74c462794da31eae0fd5db13
leekurg <anikin.idm@yandex.ru>
2022-05-28 12:40:52 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# VideoGallery #
+
+### About ###
+Swift/UIKit app for viewing and playing local video files
+
---
+++ VideoPlayer/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  VideoPlayer
+//
+//  Created by Ильяяя on 28.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .lightGray
+        
+        navigationItem.title = "Title"
+        navigationController?.navigationBar.prefersLargeTitles = true
+    }
+
+
+}
+
